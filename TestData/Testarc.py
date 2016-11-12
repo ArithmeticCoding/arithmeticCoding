6f76ae48591f5d7d5e2c0932e80873ae16d37836
zhizunyu <changy2015@my.fit.edu>
2016-11-12 12:34:45 -0500

    Add files via upload

---
+++ TestData/Testarc.py
@@ -0,0 +1,106 @@
+'''
+ - Author:  Group A
+ - Course:  CSE 4081, Fall 2016
+ - Project: Group Project, Arithmetic Coding
+ How this project is meant to be run - cmd line input
+ ArithmeticCoding.py < input.txt
+ Outputs two text files, one with the encoding and one after the decoding
+'''
+
+import sys, os
+from mpmath import *
+import time
+import psutil
+
+ENCODED_FILE_NAME = "encoded.txt"
+DECODED_FILE_NAME = "decoded.txt"
+
+model = dict()
+count = 0;
+
+def buildModel(stringToEncode):
+    charList = dict()
+    start = mpf(0)
+    global count
+    for c in stringToEncode:
+        count += 1
+        if c in charList.keys():
+            charList[c] += 1
+        else:
+            charList[c] = 1
+
+    #dps has to be longer in order to correctly encode and decode loger messages
+    #not sure how to calculate correct lenght
+
+    mp.dps = count+1
+    model.fromkeys(charList)
+    for c, v in charList.items():
+        width = mpf(charList[c]/count)
+        model[c] = (start, width)
+        start += charList[c]/count
+@profile
+def encode(stringToEncode):
+    buildModel(stringToEncode)
+    encodedFile = open(ENCODED_FILE_NAME, 'w')
+    mp.dps = count * count
+    high = mpf(1.0)
+    low = mpf(0.0)
+    global ultimateRange
+    for c in stringToEncode:
+        c_start, c_width = model[c]
+        c_end = c_start + c_width
+        d_range = mpf(high - low)
+        ultimateRange = mpf(d_range)
+        high = low + (c_end * d_range)
+        low = low + (c_start * d_range)
+
+    encodedFile.write(str(low + (high-low)/2))
+    return low + (high-low)/2
+	
+@profile
+def decode(encodedNumber):
+
+    decodedFile = open(DECODED_FILE_NAME, 'w')
+    mp.dps = count * count
+    string = []
+    high = mpf(1.0)
+    low = mpf(0.0)
+    d_range = mpf(high - low)
+    while (d_range > ultimateRange):
+        for c, (c_low, c_width) in model.items():
+            c_high = mpf(c_low + c_width)
+            d_range = mpf(high - low)
+            if c_low <= (encodedNumber - low)/d_range < c_high:
+                high = low + (d_range * c_high)
+                low = low + (d_range * c_low)
+                string.append(c)
+                decodedFile.write(c)
+                break
+    return ''.join(string)
+
+
+def main():
+    
+    stringToEncode = input()
+    startEncode = time.clock()
+    encodedNumber = encode(stringToEncode)
+    endEncode = time.clock()
+    startDecode = time.clock()
+    decodedString = decode(encodedNumber)
+    endDecode = time.clock()
+    encodeTime = endEncode - startEncode
+    decodeTime = endDecode - startDecode
+    width =0;
+    print("Message length: ", count)
+    print("Arbitrary precision: ", mp.dps)
+    print("Encoded message ", encodedNumber)
+    print("Decoded message ", decodedString)
+    print("Probability table:")
+    print("encode time is ", encodeTime)
+    print("decode time is ", decodeTime)
+    for c, (low, high) in model.items():
+        print(c, " start: ", low, " width: ", high)
+        width += high
+    print("width sum: ", width)
+if __name__ == '__main__':
+    main()
